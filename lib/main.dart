432679c4fc5f0999aac53de56ba5f011ae747640
Platton96 <sergey_kulbeda@mail.ru>
2020-01-13 12:48:33 +0300

    all work

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
       ),
     );
   }
 }
-
